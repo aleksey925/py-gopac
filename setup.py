2942377ea63f1300677e05f765e48e96c8bde99f
Aleksey Petrunnik <you@example.com>
2019-07-31 16:09:49 +0400

    Обновлен setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup, Extension
 setup(
     name='gopac',
     version='0.0.3',
-    url='',
+    url='https://bitbucket.org/alex925/gopac/src/master/',
     license='',
     author='Aleksey Petrunnik',
-    author_email='',
+    author_email='zzz_vvv.94@mail.ru',
     description='',
     packages=['gopac'],
     setup_requires=['setuptools-golang-cli'],
+    dependency_links=[
+        'git+https://alex925@bitbucket.org/alex925/setuptools-golang-cli.git@master#egg=setuptools-golang-cli'
+    ],
     ext_modules=[
         Extension(
             'gopac.extension.gopaccli',
